3b70856322c4ee63583368f7bd9484b1306ca68a
lycuong99 <lycuong99@gmail.com>
2024-06-14 21:56:20 +0700

    Create discount.model.js

---
+++ src/models/discount.model.js
@@ -0,0 +1,85 @@
+const { Types, Schema, model } = require('mongoose')
+
+const DOCUMENT_NAME = 'Discount'
+const COLLECTION_NAME = 'discounts'
+
+const discountSchema = new Schema(
+    {
+        discount_name:{
+            type: String,
+            required: true,
+        },
+        discount_code:{
+            type: String,
+            required: true,
+        },
+        discount_type:{
+            type: String,
+            default: 'fixed_amount', // fixed_amount, percentage
+        },
+        discount_value:{
+            type: Number,
+            required: true,
+        },
+        discount_description:{
+            type: String,
+            required: true,
+        },
+        discount_start_date:{
+            type: Date,
+            required: true,
+        },
+        discount_end_date:{
+            type: Date,
+            required: true,
+        },
+        discount_max_usage:{
+            type: Number,
+            required: true,
+        }, 
+        discount_usage_count:{
+            type: Number,
+            default: 0,
+        },
+        discount_max_usage_per_user:{
+            type: Number,
+            required: true,
+        },
+        discount_users_used:{
+            type: Array,
+            default: [],
+        },
+        discount_min_order_value:{
+            type: Number,
+            required: true,
+        },
+        discount_shopId:{
+            type: Schema.Types.ObjectId,
+            ref: 'Shop',
+            required: true,
+        },
+        discount_is_active:{
+            type: Boolean,
+            default: true,
+        },
+        discount_applies_to:{
+            type: String,
+            required: true,
+            enum: ['all', 'category', 'product'],
+        },
+        discount_product_ids:{
+            type: Array,
+            default: [],
+        }
+         
+
+    },
+    {
+        collection: COLLECTION_NAME,
+        timestamps: true,
+    }
+)
+
+module.exports = {
+    discount: model(DOCUMENT_NAME, discountSchema)
+}
